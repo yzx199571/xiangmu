b0c29ba6b00d57cdd67cb24480206cef3f04a91b
dalao <1160841407@qq.com>
2019-08-03 09:14:24 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 控制台提示信息  开发环境提示 详细一些
 Vue.config.productionTip = false
 
+// main.js的职责
+// 1职责导入项目需要的依赖资源（js css文件 其他资源）
+// 2职责 创建一个根实例
+
+// 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
